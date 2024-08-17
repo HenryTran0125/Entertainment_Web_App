54cb8e05f7474e939df083478c6df6cb4a3a7e1d
Henry Tran <henrytran@MacBook-Pro-cua-Tieu.local>
2024-08-17 20:33:55 +0700

    cm1708-second

--- app/ui/Recommend/Recommend.tsx
+++ app/ui/Recommend/Recommend.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./page.module.css";
 import data from "../../../data.json";
+import Image from "next/image";
 
 export default function Recommend() {
   const recommendData = data.filter((element) => !element.isTrending);
@@ -10,9 +11,33 @@ export default function Recommend() {
       <h1 className={styles.h1}>Recommended for you</h1>
 
       <div>
-        <ul>
+        <ul className={styles.ul}>
           {recommendData.map((element, index) => (
-            <li key={index}>{element.title}</li>
+            <li className={styles.li} key={index}>
+              <div>
+                <Image
+                  src={`/${element.thumbnail.regular.medium}`}
+                  width={280}
+                  height={174}
+                  alt={element.title}
+                  className={styles.img}
+                />
+              </div>
+
+              <div className={styles.information}>
+                <span className={styles.year}>{element.year}</span>
+
+                {/* <span className={styles.firstDot}></span> */}
+
+                <span className={styles.category}>{element.category}</span>
+
+                <span>{element.rating}</span>
+              </div>
+
+              <div>
+                <div>{element.title}</div>
+              </div>
+            </li>
           ))}
         </ul>
       </div>
